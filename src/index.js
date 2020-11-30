e7356f37b284b4cbaf008189b0ee537b1adcf930
voger <voger@runbox.com>
2020-11-30 22:30:34 +0200

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+var jsonPath = require("jsonpath-plus").JSONPath;
+
 var variablesObj = null;
 var variablesFile = null;
-
-
 
 function resolveVariable(variable) {
   if (variablesFile && variablesObj === null) {
@@ -10,44 +10,50 @@ function resolveVariable(variable) {
 
   var normalizedVariable = variable;
 
-  if(!normalizedVariable.startsWith("$")) {
+  if (!normalizedVariable.startsWith("$")) {
     normalizedVariable = "$." + normalizedVariable;
   }
 
-  var returnVal = null;
-  try {
-    var jsonPath = require("jsonpath-plus").JSONPath;
-    returnVal = jsonPath({path: normalizedVariable, json: variablesObj});
-  } catch(e) {
+  var returnVal = jsonPath({
+    path: normalizedVariable,
+    json: variablesObj,
+    wrap: false,
+  });
+
+  if (undefined === returnVal) {
     returnVal = "#{" + variable + "}";
   }
 
   return returnVal;
-};
+}
 
 function initializeVariablesObject() {
-  var returnVal = null;
+  var variablesObj = null;
 
-  if (variablesFile && variablesObj === null) {
+  try {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", variablesFile, false);
     xhttp.send();
 
-    returnVal = JSON.parse(xhttp.response);
+    variablesObj = JSON.parse(xhttp.response);
+  } catch (e) {
+    variablesObj = null;
   }
 
-  return returnVal;
+  return variablesObj;
 }
 
 function install(hook, vm) {
   variablesFile = vm.config.variablesFile;
-  hook.beforeEach(function(content) {
-    return content.replace(/#{(.*)}/g, function(a, b){return resolveVariable(b)})
+  hook.beforeEach(function (content) {
+    return content.replace(/#{(.*)}/g, function (a, b) {
+      return resolveVariable(b);
+    });
   });
 }
 
 if (!window.$docsify) {
-  window.$docsify = {}
+  window.$docsify = {};
 }
 
-window.$docsify.plugins = (window.$docsify.plugins || []).concat(install)
+window.$docsify.plugins = (window.$docsify.plugins || []).concat(install);
